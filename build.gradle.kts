0933eebaf8e3bc12ed56e53907ff1abd18cb2c24
Brandon Fergerson <bfergerson@apache.org>
2021-12-25 20:06:39 -0500

    deserialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
     implementation("com.github.sourceplusplus.protocol:protocol:$protocolVersion")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.slf4j:slf4j-nop:1.7.32")

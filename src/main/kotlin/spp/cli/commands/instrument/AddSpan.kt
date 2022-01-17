5a827d43288755fc233179f4f8d3ba6630c08b0d
Brandon Fergerson <bfergerson@apache.org>
2022-01-17 15:05:56 -0500

    latest

--- src/main/kotlin/spp/cli/commands/instrument/AddSpan.kt
+++ src/main/kotlin/spp/cli/commands/instrument/AddSpan.kt
@@ -1,48 +1,39 @@
 package spp.cli.commands.instrument
 
 import com.apollographql.apollo3.api.CustomScalarAdapters
-import com.apollographql.apollo3.api.Optional
 import com.apollographql.apollo3.api.json.MapJsonWriter
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.enum
-import com.github.ajalt.clikt.parameters.types.int
-import com.github.ajalt.clikt.parameters.types.long
 import kotlinx.coroutines.runBlocking
 import spp.cli.Main
 import spp.cli.PlatformCLI.apolloClient
 import spp.cli.PlatformCLI.echoError
 import spp.cli.protocol.instrument.AddLiveSpanMutation
 import spp.cli.protocol.instrument.adapter.AddLiveSpanMutation_ResponseAdapter.AddLiveSpan
-import spp.cli.protocol.type.InstrumentThrottleInput
-import spp.cli.protocol.type.LiveSourceLocationInput
 import spp.cli.protocol.type.LiveSpanInput
-import spp.cli.protocol.type.ThrottleStep
+import spp.cli.protocol.type.LiveSpanLocationInput
 import spp.cli.util.JsonCleaner
 import kotlin.system.exitProcess
 
 class AddSpan : CliktCommand() {
 
-    val source by argument(help = "Qualified class name")
-    val line by argument(help = "Line number").int()
+    val source by argument(help = "Qualified function name")
     val operationName by argument(help = "Operation name")
-    val condition by option("-condition", "-c", help = "Trigger condition")
-    val expiresAt by option("-expiresAt", "-e", help = "Expiration time (epoch time [ms])").long()
-    val hitLimit by option("-hitLimit", "-h", help = "Trigger hit limit").int()
-    val throttleLimit by option("-throttleLimit", "-t", help = "Trigger throttle limit").int().default(1)
-    val throttleStep by option("-throttleStep", "-s", help = "Trigger throttle step").enum<ThrottleStep>()
-        .default(ThrottleStep.SECOND)
+//    val condition by option("-condition", "-c", help = "Trigger condition")
+//    val expiresAt by option("-expiresAt", "-e", help = "Expiration time (epoch time [ms])").long()
+//    val hitLimit by option("-hitLimit", "-h", help = "Trigger hit limit").int()
+//    val throttleLimit by option("-throttleLimit", "-t", help = "Trigger throttle limit").int().default(1)
+//    val throttleStep by option("-throttleStep", "-s", help = "Trigger throttle step").enum<ThrottleStep>()
+//        .default(ThrottleStep.SECOND)
 
     override fun run() = runBlocking {
         val input = LiveSpanInput(
             operationName = operationName,
-            location = LiveSourceLocationInput(source, line),
-            condition = Optional.Present(condition),
-            expiresAt = Optional.Present(expiresAt),
-            hitLimit = Optional.Present(hitLimit),
-            throttle = Optional.Present(InstrumentThrottleInput(throttleLimit, throttleStep))
+            location = LiveSpanLocationInput(source),
+//            condition = Optional.Present(condition),
+//            expiresAt = Optional.Present(expiresAt),
+//            hitLimit = Optional.Present(hitLimit),
+//            throttle = Optional.Present(InstrumentThrottleInput(throttleLimit, throttleStep))
         )
         val response = try {
             apolloClient.mutation(AddLiveSpanMutation(input)).execute()

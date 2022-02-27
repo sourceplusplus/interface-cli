82b997760a4ff968a99c831323e0c01a27e98922
Brandon Fergerson <bfergerson@apache.org>
2022-02-27 21:35:44 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
     api("com.apollographql.apollo3:apollo-api:$apolloVersion")
 
-    implementation("com.github.sourceplusplus.protocol:protocol:$projectVersion")
+    implementation("com.github.sourceplusplus.protocol:protocol:2bd4bea9ad")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")

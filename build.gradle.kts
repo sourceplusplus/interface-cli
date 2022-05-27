f92e3da5c3ac30f4324f77f8d622dca242678c72
Brandon Fergerson <bfergerson@apache.org>
2022-05-27 18:35:09 +0400

    fix dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
     api("com.apollographql.apollo3:apollo-api:$apolloVersion")
 
-    implementation("plus.sourceplusplus:protocol:$projectVersion")
+    implementation("plus.sourceplus:protocol:$projectVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")

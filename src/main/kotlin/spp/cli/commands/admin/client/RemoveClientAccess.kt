a987512d8729d4ef83b9a4d713df29da8081d92f
UltraDev <petzmagnus@gmail.com>
2022-08-24 15:59:16 +0200

    Add client access management commands (#95)

---
+++ src/main/kotlin/spp/cli/commands/admin/client/RemoveClientAccess.kt
@@ -0,0 +1,47 @@
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package spp.cli.commands.admin.client
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import kotlinx.coroutines.runBlocking
+import spp.cli.Main
+import spp.cli.PlatformCLI.apolloClient
+import spp.cli.PlatformCLI.echoError
+import spp.cli.protocol.client.RemoveClientAccessMutation
+import kotlin.system.exitProcess
+
+class RemoveClientAccess : CliktCommand(printHelpOnEmptyArgs = true) {
+
+    val id by argument(help = "Client Access ID")
+
+    override fun run() = runBlocking {
+        val response = try {
+            apolloClient.mutation(RemoveClientAccessMutation(id)).execute()
+        } catch (e: Exception) {
+            echoError(e)
+            if (Main.standalone) exitProcess(-1) else return@runBlocking
+        }
+        if (response.hasErrors()) {
+            echo(response.errors?.get(0)?.message, err = true)
+            if (Main.standalone) exitProcess(-1) else return@runBlocking
+        }
+
+        echo(response.data!!.removeClientAccess)
+        if (Main.standalone) exitProcess(0)
+    }
+}
